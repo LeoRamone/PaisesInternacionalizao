058eb18a4130c5470e4ea24eeec0ba61680d0ee1
Leonardo Soares Santos | IT Trainee <leonardo_soares@gsaa.saa.sp.gov.br>
2018-04-24 17:17:00 -0300

    Initial commit

---
+++ app/src/main/java/com/leonardo_soares_santos/paisesinternacionalizao/Util.java
@@ -0,0 +1,27 @@
+package com.leonardo_soares_santos.paisesinternacionalizao;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by asbonato on 22/09/17.
+ */
+
+public class Util {
+    public static Drawable getDrawable(Context context, String nome){
+
+        Class<?> c = R.drawable.class;
+        try {
+            Field idField = c.getDeclaredField(nome);
+            int id = idField.getInt(idField);
+            return context.getResources().getDrawable(id, null);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
---
+++ app/src/main/java/com/leonardo_soares_santos/paisesinternacionalizao/ViewHolder.java
@@ -0,0 +1,44 @@
+package com.leonardo_soares_santos.paisesinternacionalizao;
+
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by asbonato on 22/09/17.
+ */
+
+public class ViewHolder {
+    ImageView bandeira;
+    TextView nome, detalhe;
+
+    public ViewHolder(ImageView bandeira, TextView nome, TextView detalhe) {
+        this.bandeira = bandeira;
+        this.nome = nome;
+        this.detalhe = detalhe;
+    }
+
+    public ImageView getBandeira() {
+        return bandeira;
+    }
+
+    public void setBandeira(ImageView bandeira) {
+        this.bandeira = bandeira;
+    }
+
+    public TextView getNome() {
+        return nome;
+    }
+
+    public void setNome(TextView nome) {
+        this.nome = nome;
+    }
+
+    public TextView getDetalhe() {
+        return detalhe;
+    }
+
+    public void setDetalhe(TextView detalhe) {
+        this.detalhe = detalhe;
+    }
+}
